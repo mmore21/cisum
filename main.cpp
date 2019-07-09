bfa5fef8b651fe9ed19210b856d4d0839cc9bbf6
Mason Moreland <mazc@protonmail.ch>
2019-07-08 22:39:44 -0700

    Add fonts

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    if (QFontDatabase::addApplicationFont(":/fonts/fonts/Bebas.otf") < 0)
+        qWarning() << "Bebas cannot be loaded !";
+
+    if (QFontDatabase::addApplicationFont(":/fonts/fonts/FontAwesome.otf") < 0)
+        qWarning() << "FontAwesome cannot be loaded !";
+
+    if (QFontDatabase::addApplicationFont(":/fonts/fonts/Rezland.ttf") < 0)
+        qWarning() << "Rezland cannot be loaded !";
 
     return a.exec();
 }
